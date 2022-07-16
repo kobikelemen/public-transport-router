a7913761f5b10ae192f717edad28eeba19aadef4
kobikelemen <kobi.kelemen20@gmail.com>
2022-07-16 13:14:27 +0100

    fixed

--- test.c
+++ test.c
@@ -32,23 +32,50 @@ int read_file(int num_lines, char * filename, char *(*text[]), int capital)
 int main()
 {
 
-    int num_1 = 100;
+
+
+    int num_1 = 120;
     char * bus1[num_1];
+
+    char bus_routes[num_1][1];
+    char arrival_mins[num_1][2];
+    char bsnames[num_1][100];
 
     read_file(num_1, "data_processing/bus_times/1.txt", &bus1, 0);
 
+
+    int b1 = 1;
+    int b2 = 9;
+    int endlen;
+
     for (int i=0; i < num_1; i ++) {
-        printf("\n%s", bus1[i]);
+        // printf("\n%s", bus1[i]);
+
+        memcpy(bus_routes[i], &(bus1[i][0]), sizeof(*(bus1[i])));
+        memcpy(arrival_mins[i], &(bus1[i][4]), 2 * sizeof(*(bus1[i])) );
+
+
+        // char *p = & (*bus1[i]);
+        // for (int j=0; j < 10; j ++) {
+        //     p++; // now points to start of name part
+        // }
+
+        // endlen = 0;
+
+        // while (strcmp(*p, "\0") != 0) {
+        //     p++;
+        //     endlen++;
+        // }
+
+
+
+        memcpy(bsnames[i], &(bus1[i][10]), (strlen(bus1[i]) - 10) * sizeof(*(bus1[i])));
+
+        printf("\nroute: %s", bus_routes[i]);
+        printf("\nmins: %i", atoi(arrival_mins[i]));
+        // char * t = bsnames[i];
+        printf("\nname:%s", bsnames[i]);
+
+        
     }
-
-
-    // int len_routes = 100;
-    // char *busstop_names_routes[len_routes];
-    // read_file(len_routes, "bus_sequences/stop_name.txt", &busstop_names_routes, 1);
-    // char name[50];
-
-    // for ( int i=0 ; i < 10 ; i ++ ) {
-    //     memcpy(name, &(bus1[i][10]), sizeof(bus1[i]) - 9 * sizeof(*(bus1[i])) );
-    //     printf("\n%s", name);
-    // }
 }
