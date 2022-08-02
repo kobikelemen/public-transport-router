50a50477e80ae0b8c0655d83f1e1f549313018fe
kobikelemen <kobi.kelemen20@gmail.com>
2022-08-02 18:29:23 +0100

    search address autocomplete

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import subprocess
+import os 
+import sys
+import inspect
+# from ... import requests
 
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from req import imgrequest
 
 starte = '532733'
 startn = '176014'
@@ -13,6 +22,8 @@ subprocess.call(["./main", starte, startn, ende, endn])
 f = open('frontend/route.txt', 'r')
 route = f.readlines()
 print(route)
+ 
+imgrequest.img()
 
 """
 pass start + end coords to dijkstras
