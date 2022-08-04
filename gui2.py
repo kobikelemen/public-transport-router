263772b69c057c4c2f1a649923a38bab4db1049b
kobikelemen <kobi.kelemen20@gmail.com>
2022-08-04 18:07:39 +0100

    checkpoint

--- gui2.py
+++ gui2.py
@@ -161,24 +161,31 @@ class AutocompleteEntry(Entry):
         # return [ w for w in self.autocompleteList if self.matchesFunction(self.var.get(), w) ]
 
 if __name__ == '__main__':
+
+    def on_search(entry1, entry2):
+        
+
+
     autocompleteList = [ 'Dora Lyons (7714)', 'Hannah Golden (6010)', 'Walker Burns (9390)', 'Dieter Pearson (6347)', 'Allen Sullivan (9781)', 'Warren Sullivan (3094)', 'Genevieve Mayo (8427)', 'Igor Conner (4740)', 'Ulysses Shepherd (8116)', 'Imogene Bullock (6736)', 'Dominique Sanchez (949)', 'Sean Robinson (3784)', 'Diana Greer (2385)', 'Arsenio Conrad (2891)', 'Sophia Rowland (5713)', 'Garrett Lindsay (5760)', 'Lacy Henry (4350)', 'Tanek Conley (9054)', 'Octavia Michael (5040)', 'Kimberly Chan (1989)', 'Melodie Wooten (7753)', 'Winter Beard (3896)', 'Callum Schultz (7762)', 'Prescott Silva (3736)', 'Adena Crane (6684)', 'Ocean Schroeder (2354)', 'Aspen Blevins (8588)', 'Allegra Gould (7323)', 'Penelope Aguirre (7639)', 'Deanna Norman (1963)', 'Herman Mcintosh (1776)', 'August Hansen (547)', 'Oscar Sanford (2333)', 'Guy Vincent (1656)', 'Indigo Frye (3236)', 'Angelica Vargas (1697)', 'Bevis Blair (4354)', 'Trevor Wilkinson (7067)', 'Kameko Lloyd (2660)', 'Giselle Gaines (9103)', 'Phyllis Bowers (6661)', 'Patrick Rowe (2615)', 'Cheyenne Manning (1743)', 'Jolie Carney (6741)', 'Joel Faulkner (6224)', 'Anika Bennett (9298)', 'Clayton Cherry (3687)', 'Shellie Stevenson (6100)', 'Marah Odonnell (3115)', 'Quintessa Wallace (5241)', 'Jayme Ramsey (8337)', 'Kyle Collier (8284)', 'Jameson Doyle (9258)', 'Rigel Blake (2124)', 'Joan Smith (3633)', 'Autumn Osborne (5180)', 'Renee Randolph (3100)', 'Fallon England (6976)', 'Fallon Jefferson (6807)', 'Kevyn Koch (9429)', 'Paki Mckay (504)', 'Connor Pitts (1966)', 'Rebecca Coffey (4975)', 'Jordan Morrow (1772)', 'Teegan Snider (5808)', 'Tatyana Cunningham (7691)', 'Owen Holloway (6814)', 'Desiree Delaney (272)', 'Armand Snider (8511)', 'Wallace Molina (4302)', 'Amela Walker (1637)', 'Denton Tillman (201)', 'Bruno Acevedo (7684)', 'Slade Hebert (5945)', 'Elmo Watkins (9282)', 'Oleg Copeland (8013)', 'Vladimir Taylor (3846)', 'Sierra Coffey (7052)', 'Holmes Scott (8907)', 'Evelyn Charles (8528)', 'Steel Cooke (5173)', 'Roth Barrett (7977)', 'Justina Slater (3865)', 'Mara Andrews (3113)', 'Ulla Skinner (9342)', 'Reece Lawrence (6074)', 'Violet Clay (6516)', 'Ainsley Mcintyre (6610)', 'Chanda Pugh (9853)', 'Brody Rosales (2662)', 'Serena Rivas (7156)', 'Henry Lang (4439)', 'Clark Olson (636)', 'Tashya Cotton (5795)', 'Kim Matthews (2774)', 'Leilani Good (5360)', 'Deirdre Lindsey (5829)', 'Macy Fields (268)', 'Daniel Parrish (1166)', 'Talon Winters (8469)' ]
     def matches(fieldValue, acListEntry):
         pattern = re.compile(re.escape(fieldValue) + '.*', re.IGNORECASE)
         return re.match(pattern, acListEntry)
     
     root = Tk()
-    l = Label().grid(column=0)
+    root.title('London Public Transport Router')
+    l = Label().grid(column=1)
     m = tkintermapview.TkinterMapView(l, width=800, height=600)
     m.set_position(51.475331432, -0.170165986)
-
     entry = AutocompleteEntry(m, autocompleteList, root, listboxLength=6, width=32, matchesFunction=matches)
     entry.grid(row=0, column=0)
+    # x0 = StringVar()
+    # x0.set('From')
+    # l0 = Label(root, textvariable=x0)
+    # l0.grid(row=0, column=0)
+    entry2 = AutocompleteEntry(m, autocompleteList, root, listboxLength=6, width=32, matchesFunction=matches)
+    entry2.grid(row=1, column=0)
     m.grid(column=0)
-    # m.set_path([(51.475331432, -0.170165986),(51.482719,-0.174342),(51.489049,-0.164969),(51.495840,-0.152248) ])
-    print('bs', entry.bs_coords)
-    if entry.bs_coords != []:
-        path = m.set_path(entry.bs_coords)
-    # m.set_position(48.86, 2.338)
+    searchbtn = Button(root, text='Route', command=on_search(entry, entry2))
     
     
     root.mainloop()
